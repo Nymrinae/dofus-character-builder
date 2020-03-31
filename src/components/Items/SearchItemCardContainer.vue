de72ca8319519c462a0b07629451393e51d3bebe
Nymrinae <nymrinaedev@gmail.com>
2020-03-31 23:56:32 +0200

    Add auto-completion to items

--- src/components/Items/SearchItemCardContainer.vue
+++ src/components/Items/SearchItemCardContainer.vue
@@ -1,9 +1,9 @@
 <template>
   <NoItemSelectedCard v-if="!currentActiveItemType" />
-  <!-- insert SelectItemCard here instead -->
-  <v-card v-else class="mt-6" width="400" height="765" outlined>
-    <v-subheader v-text="currentActiveItemType" />
-  </v-card>
+  <SelectedItemCard
+    v-else
+    :type="currentActiveItemType"
+  />
 </template>
 
 <script>
--- src/components/Items/SelectedItemCard.vue
+++ src/components/Items/SelectedItemCard.vue
@@ -1,10 +1,37 @@
 <template>
-  <p />
+  <v-card class="mt-6" width="400" height="765" outlined>
+    <v-container>
+      <v-card-text
+        class="headline font-weight-black pa-6"
+        v-text="type"
+      />
+      <v-card-text>
+        <v-autocomplete
+          :items="itemNames"
+          color="white"
+          :placeholder="`Cherche ton ${type} ici !`"
+          prepend-icon="mdi-magnify"
+        />
+      </v-card-text>
+      <v-card-text> test </v-card-text>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
 
+export default {
+  props: {
+    type: { type: String, required: true }
+  },
+  computed: {
+    ...mapState('build', ['currentItems']),
+    itemNames() { return this.currentItems.map(e => e.name) }
+  },
+  mounted() {
+    console.log(this.itemNames)
+  }
 }
 </script>
 
